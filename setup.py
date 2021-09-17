4d105d15eaf9e55d3c3b40e0fdb53b4a7d4f3443
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2021-09-17 00:01:37 -0500

    Add setup for abi in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
-from setuptools import setup, Extension
+import platform
+import sys
+from setuptools import setup
+from setuptools import Extension
+
+
+py_major_version, py_minor_version, *_ = sys.version_info
+if py_major_version >= 3 and platform.python_implementation() == 'CPython':
+    try:
+        import wheel.bdist_wheel
+    except ImportError:
+        command_class = {}
+    else:
+        class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
+            def finalize_options(self):
+                self.py_limited_api = f'cp3{py_minor_version}'
+                super().finalize_options()
+
+        command_class = {'bdist_wheel': bdist_wheel}
 
 
 setup(
     name='rename-exchange',
     version='1.0.0',
     ext_modules=[
-        Extension('rename_exchange', ['rename_exchange.c']),
+        Extension(
+            'rename_exchange', ['rename_exchange.c'],
+            py_limited_api=True, define_macros=[('PY_LIMITED_API', None)],
+        ),
     ],
+    cmdclass=command_class,
 )
